d540293170146b0a5f4c1b1941e6f991e69a21c2
jaewon <jaewon@wishket.com>
2022-07-08 15:26:19 +0900

    ?

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 sudo cp -r /home/ubuntu/run/ /home/ubuntu/sourcecode/blog
-sudo cp -r /home/ubuntu/sourcecode/blog/scripts/blog /etc/nginx/sites-available/
+# sudo cp -r /home/ubuntu/sourcecode/blog/scripts/blog /etc/nginx/sites-available/
