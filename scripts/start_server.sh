bb8268885fe859c7c5a4707e337b646946fccc38
jaewon <jaewon@wishket.com>
2022-07-12 11:48:17 +0900

    오우타

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo cp -r /home/ubuntu/sourcecode/blog/scripts/blog /etc/nginx/site-available/
+sudo cp -r /home/ubuntu/sourcecode/blog/scripts/blog /etc/nginx/sites-available/
 
 sudo service gunicorn restart
 sudo service nginx restart
