54580b4d703287f4fe28caf25fe5bf4e46dadac5
jaewon <jaewon@wishket.com>
2022-07-13 10:06:08 +0900

    add -y

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -3,6 +3,6 @@
 
 cd /home/ubuntu/sourcecode/blog/
 # sudo rm -rf /home/ubuntu/sourcecode/blog/
-sudo apt-get install python3-pip
+sudo apt-get install -y python3-pip
 
 sudo pip install -r requirement.txt
