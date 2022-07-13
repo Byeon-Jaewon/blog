511b8000bee539368ecbe588dd522ecfc62e81b9
jaewon <jaewon@wishket.com>
2022-07-13 10:31:01 +0900

    test

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -3,7 +3,5 @@
 
 cd /home/ubuntu/sourcecode/blog/
 # sudo rm -rf /home/ubuntu/sourcecode/blog/
-sudo apt-get install -y python-setuptools
-sudo easy_install pip
 
-sudo pip install -r requirement.txt
+sudo /home/ubuntu/.pyenv/shims/pip install -r requirement.txt
