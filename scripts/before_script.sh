2665b8426b0ff5e64a1faf797f666c39b0f2e0d3
jaewon <jaewon@wishket.com>
2022-07-13 10:18:10 +0900

    easy_install

--- scripts/before_script.sh
+++ scripts/before_script.sh
@@ -3,6 +3,7 @@
 
 cd /home/ubuntu/sourcecode/blog/
 # sudo rm -rf /home/ubuntu/sourcecode/blog/
-sudo apt-get install -y python3-pip
+sudo apt-get install -y python-setuptools
+sudo easy_install pip
 
 sudo pip install -r requirement.txt
